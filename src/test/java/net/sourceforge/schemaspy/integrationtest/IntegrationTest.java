e17a4d768068d76369d1070deae5f16ea03699bd
Thomas Philipona <philipona@puzzle.ch>
2015-12-13 10:43:18 +0100

    implemented basic Integration Test for test database h2

--- src/test/java/net/sourceforge/schemaspy/integrationtest/IntegrationTest.java
+++ src/test/java/net/sourceforge/schemaspy/integrationtest/IntegrationTest.java
@@ -1,10 +1,16 @@
 package net.sourceforge.schemaspy.integrationtest;
 
+import net.sourceforge.schemaspy.Config;
+import net.sourceforge.schemaspy.SchemaAnalyzer;
+import net.sourceforge.schemaspy.model.Database;
 import org.h2.tools.DeleteDbFiles;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.sql.*;
+
+import static org.junit.Assert.assertNotNull;
 
 /**
  */
@@ -27,7 +33,30 @@ public class IntegrationTest {
     }
 
     @Test
-    public void test() {
+    public void test() throws IOException, SQLException {
+        // given
+        SchemaAnalyzer analyzer = new SchemaAnalyzer();
+        String[] argv = new String[12];
+        argv[0] = "-t";
+        argv[1] = "h2";
+        argv[2] = "-host";
+        argv[3] = "localhost";
+        argv[4] = "-db";
+        argv[5] = "./src/test/resources/testdb/testdb";
+        argv[6] = "-u";
+        argv[7] = "";
+        argv[8] = "-p";
+        argv[9] = "";
+        argv[10] = "-o";
+        argv[11] = "./src/test/resources/testdb/output";
+
+                //java -jar schemaSpy_5.0.0.jar -t orathin -host sitaki.rz.puzzle.ch -port 1521 -db PITC01 -u <USER> -p <pw> -s <SCHEMA> -o ./output/ -dp driver.jar
+
+        // when
+        Database result = analyzer.analyze(new Config(argv));
+
+        // then
+        assertNotNull(result);
 
     }
 }
