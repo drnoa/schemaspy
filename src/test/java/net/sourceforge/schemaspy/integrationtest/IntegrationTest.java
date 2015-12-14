f731d65b037b52901122571b6ffca43300cb63fa
Thomas Philipona <philipona@puzzle.ch>
2015-12-14 15:33:39 +0100

    added some more assertions

--- src/test/java/net/sourceforge/schemaspy/integrationtest/IntegrationTest.java
+++ src/test/java/net/sourceforge/schemaspy/integrationtest/IntegrationTest.java
@@ -3,12 +3,14 @@ package net.sourceforge.schemaspy.integrationtest;
 import net.sourceforge.schemaspy.Config;
 import net.sourceforge.schemaspy.SchemaAnalyzer;
 import net.sourceforge.schemaspy.model.Database;
+import net.sourceforge.schemaspy.model.Table;
 import org.h2.tools.DeleteDbFiles;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -37,7 +39,7 @@ public class IntegrationTest {
     public void test() throws IOException, SQLException {
         // given
         SchemaAnalyzer analyzer = new SchemaAnalyzer();
-        String[] argv = new String[12];
+        String[] argv = new String[14];
         argv[0] = "-t";
         argv[1] = "h2";
         argv[2] = "-host";
@@ -50,6 +52,8 @@ public class IntegrationTest {
         argv[9] = "";
         argv[10] = "-o";
         argv[11] = "./src/test/resources/testdb/output";
+        argv[12] = "-meta";
+        argv[13] = "./src/test/resources/test.meta.xml";
 
         // when
         Database result = analyzer.analyze(new Config(argv));
@@ -57,7 +61,22 @@ public class IntegrationTest {
         // then
         assertNotNull(result);
         assertEquals(9, result.getTables().size());
+        assertEquals(5, getTableByName(result.getTables(), "AUTHOR").getColumns().size());
+        assertEquals("VARCHAR", getTableByName(result.getTables(), "AUTHOR").getColumn("FIRSTNAME").getTypeName());
+        assertEquals(80, getTableByName(result.getTables(), "BOOK").getNumRows());
+
+        assertEquals(1, getTableByName(result.getTables(), "AUTHOR").getAdditionalInfo().size());
+        assertEquals("http://google.ch", getTableByName(result.getTables(), "AUTHOR").getAdditionalInfo().iterator().next().getValue());
         // TODO also check generated Files under ./src/test/resources/testdb/output
 
+    }
+
+    private Table getTableByName(Collection<Table> tables, String name){
+        for (Table table : tables) {
+            if(name.equalsIgnoreCase(table.getName())){
+                return table;
+            }
+        }
+        return null;
     }
 }
