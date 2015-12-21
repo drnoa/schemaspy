5a01bbf8b56ddfbdbe295cb596d87f9bff7fffdf
Thomas Philipona <philipona@puzzle.ch>
2015-12-21 16:59:16 +0100

    fixed Junittest

--- src/test/java/net/sourceforge/schemaspy/view/HtmlTablePageTest.java
+++ src/test/java/net/sourceforge/schemaspy/view/HtmlTablePageTest.java
@@ -53,9 +53,7 @@ public class HtmlTablePageTest {
 		
 		//then
 		String expected = readFile("src/test/resources/templates/testtemplates/expectedresults/TableResult1.html");
-
-
-
+		
 		assertEquals(expected, result);
 	}
 
